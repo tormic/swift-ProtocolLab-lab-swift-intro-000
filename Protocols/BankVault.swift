5ee3ff25fc599901b2a9879bedfd09288899d63d
Tor Magnus Michaelsen <tormagnusmichaelsen@Tors-MacBook-Pro.local>
2018-04-07 14:08:46 +0200

    correct solution, but getting build errors, modules missing

--- Protocols/BankVault.swift
+++ Protocols/BankVault.swift
@@ -20,7 +20,36 @@ class BankVault {
     
 }
 
+//protocol named ProvideAcces
+protocol ProvideAccess {
+    
+    //define header for function but nothing else
+    func allowEntryWithPassword(password: [Int]) ->Bool
+}
 
+extension BankVault {
+    func allowEntryWithPassword(password: [Int]) -> Bool {
+        var allow = false
+        if (password.count == 0 || password.count > 10) {
+            return allow
+            } else {
+            allow = true
+            for digit in password {
+                if (digit % 2 != 0) {
+                    allow = false
+                }
+            }
+        return allow
+        }
+    }
+}
 
+/*
 
+ (2) - In the BankVault.swift file, below where you created the ProvideAccess protocol - create an extension on the BankVault class. In that extension, it should conform to the ProvideAccess protocol. In your implementation of the allowEntryWithPassword(_:) function, you should adhere to the following rules:
+ If the person calling on this function doesn't enter in any digits (empty array), then we should return false.
+ If the total numbers of digits entered (items in the array) exceed 10, then we should return false.
+ In order to allow access (return true), every other digit (starting with the first digit) must be even. The first digit entered is the first element in the array.
+ There are tests in the BankVaultTests.swift file. Press command + u to run these tests to make sure you implemented the allowEntryWithPassword(_:) function correctly.
+ */
 
--- Protocols/Ships.swift
+++ Protocols/Ships.swift
@@ -7,7 +7,30 @@
 //
 
 
+enum Direction: String {
+    case north, south, east, west
+}
 
+protocol Steerability {
+    var direction: Direction {
+        get set //gettable and settable
+    }
+    mutating func steerInDirection(direction: Direction) //mutating since it might be a struct
+}
+
+extension Steerability {
+    mutating func steerInDirection(direction: Direction) {
+        self.direction = direction //object calling on the function's direction will be set to direction called with
+    }
+}
+
+class SailBoat: Steerability {
+    var direction: Direction = .north //usually have to provide an initializer, but not in this case since all properties are set to an actual value
+}
+
+class rowBoat: Steerability {
+    var direction: Direction = .north
+}
 
 
 
